1f43741093c516962b41fac2734525394ce9addf
Sanjin Kurelić <kurelic@sanjin.eu>
2019-04-26 16:13:48 +0200

    Reconfigured p6spy - only show errors, no sql. Fixed bux fix in CityDescription meta model.

--- CapitalBusConnectRepository/src/main/java/eu/sanjin/kurelic/cbc/repo/entity/CityDescription_.java
+++ CapitalBusConnectRepository/src/main/java/eu/sanjin/kurelic/cbc/repo/entity/CityDescription_.java
@@ -11,7 +11,7 @@ public class CityDescription_ {
     public static volatile SingularAttribute<CityDescription, LanguagePrimaryKey> id;
     public static volatile SingularAttribute<CityDescription, String> title;
     public static volatile SingularAttribute<CityDescription, String> description;
-    public static volatile SingularAttribute<CityDescription, City> City;
+    public static volatile SingularAttribute<CityDescription, City> city;
     public static volatile SingularAttribute<CityDescription, CountryDescription> country;
 
 }
