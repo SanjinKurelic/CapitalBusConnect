6446a4c6b8a7402d136725bc22ddeb000edfd968
Sanjin Kurelić <kurelic@sanjin.eu>
2019-05-03 20:29:42 +0200

    Fixed CSS for mobile version of site.

--- CapitalBusConnectPresentation/gulpfile.js
+++ CapitalBusConnectPresentation/gulpfile.js
@@ -1,44 +1,46 @@
-/*
- * Created by Sanjin Kurelic (kurelic@sanjin.eu)
- */
+/* Created by Sanjin Kurelic (kurelic@sanjin.eu) */
+
+/*global require*/
 
 var
     // Main
-    gulp = require('gulp'),
-    concat = require('gulp-concat'),
+    gulp = require("gulp"),
+    concat = require("gulp-concat"),
     // Config
-    resourceDirectory = 'src/main/webapp/resources/',
-    jsDirectory = resourceDirectory + 'gjs/*.js',
+    resourceDirectory = "src/main/webapp/resources/",
+    jsDirectory = resourceDirectory + "gjs/*.js",
     sassDirectory = [
-        resourceDirectory + 'scss/assets/*.scss',
-        resourceDirectory + 'scss/*.scss'
+        resourceDirectory + "scss/assets/*.scss",
+        resourceDirectory + "scss/*.scss"
     ],
     // Js
-    uglify = require('gulp-uglify'),
+    uglify = require("gulp-uglify"),
     // Css
-    sass = require('gulp-sass'),
-    autoprefixer = require('autoprefixer'),
-    postcss = require('gulp-postcss')
-;
+    sass = require("gulp-sass"),
+    autoprefixer = require("autoprefixer"),
+    postcss = require("gulp-postcss");
 
-gulp.task('scripts', function () {
-
+gulp.task("scripts", function () {
+    "use strict";
     gulp.src(jsDirectory)
-        .pipe(concat('script.js'))
-        //.pipe(uglify())
-        .pipe(gulp.dest(resourceDirectory + 'js'));
+        .pipe(concat("script.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest(resourceDirectory + "js"));
 
 });
 
-gulp.task('style', function () {
+gulp.task("style", function () {
+    "use strict";
+    // noinspection JSUnresolvedFunction
     gulp.src(sassDirectory)
-        .pipe(concat('style.scss'))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([autoprefixer({ remove:false })]))
-        .pipe(gulp.dest(resourceDirectory + 'css'));
+        .pipe(concat("style.scss"))
+        .pipe(sass().on("error", sass.logError))
+        .pipe(postcss([autoprefixer({remove: false})]))
+        .pipe(gulp.dest(resourceDirectory + "css"));
 });
 
-gulp.task('default', function () {
-    gulp.watch(sassDirectory, ['style']);
-    gulp.watch(jsDirectory, ['scripts']);
+gulp.task("default", function () {
+    "use strict";
+    gulp.watch(sassDirectory, ["style"]);
+    gulp.watch(jsDirectory, ["scripts"]);
 });
