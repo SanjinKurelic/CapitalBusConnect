2c0f71d9affee3fe79b42977a73536be1aeb1acd
Sanjin Kurelić <kurelic@sanjin.eu>
2020-11-01 09:38:48 +0100

    Spring initializer structure

---
+++ src/main/java/eu/sanjin/kurelic/cbc/CapitalBusConnectApplication.java
@@ -0,0 +1,13 @@
+package eu.sanjin.kurelic.cbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalBusConnectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CapitalBusConnectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/eu/sanjin/kurelic/cbc/CapitalBusConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.sanjin.kurelic.cbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalBusConnectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
